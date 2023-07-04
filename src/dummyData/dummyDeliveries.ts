2d9b11804a3032959ad75d2be2e4a7bac3f87568
Seth Baker <seth.baker@dish.com>
2023-07-04 09:53:40 -0500

    initial setup for most types, node, and express

---
+++ src/dummyData/dummyDeliveries.ts
@@ -0,0 +1,32 @@
+import type { Delivery } from "../types/deliveries";
+
+const dummyDeliveries: Delivery[] = [
+  {
+    uuid: "1",
+    duration: 500,
+    transportVehicles: ["Truck"],
+    size: {
+      width: 12,
+      height: 12,
+      length: 12,
+    },
+    weight: 5,
+    fragile: false,
+    perishable: false,
+  },
+  //   {
+  //     uuid: "2",
+  //     duration: 500,
+  //     transportVehicles: ["Truck", "Plane"],
+  //     size: {
+  //       width: 24,
+  //       height: 24,
+  //       length: 5,
+  //     },
+  //     weight: 12,
+  //     fragile: false,
+  //     perishable: false,
+  //   },
+];
+
+export default dummyDeliveries;
---
+++ src/types/vehicles.ts
@@ -0,0 +1,13 @@
+/**
+ * `TransportVehicles` type, used as a subtype for the type of a vehicles.
+ */
+export type TransportVehicles = "Truck" | "Plane";
+
+/**
+ * `Vehicles` type, used as a type for vehicles.
+ */
+export type Vehicle = {
+  type: TransportVehicles;
+  duration: number;
+  numAvailable: number;
+};
